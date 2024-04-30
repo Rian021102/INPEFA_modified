a7716a03c18cfac126343944fa345bac5f2d100e
Rian Rachmanto <108880724+Rian021102@users.noreply.github.com>
2024-04-30 15:11:18 +0800

    changing code lines on main app

--- app2.py
+++ app2.py
@@ -16,9 +16,8 @@ st.write('This app is a Streamlit implementation of the INPEFA method for well l
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # Visualization function
-def vislog(inpefa, x, filename):
+def vislog(inpefa, x):
     plt.figure(figsize=(18, 16), dpi=80, facecolor='w', edgecolor='k')
-    plt.figtext(0.1, 0.95, filename, fontsize=12, ha='left')  # Adding the file name at the top left
     titles = ['Original GR Curve', 'Long Term INPEFA', 'Mid Term INPEFA', 'Short Term INPEFA', 'Shorter Term INPEFA']
     for i, key in enumerate(['OG', '1', '2', '3', '4']):
         plt.subplot(1, 5, i+1)
@@ -32,14 +31,13 @@ def vislog(inpefa, x, filename):
     st.pyplot()
 
 def process_file(file):
-    # Create a temporary file to store the LAS data
     with tempfile.NamedTemporaryFile(delete=False, suffix='.las') as tmpfile:
         tmpfile.write(file.getvalue())
         tmpfile.seek(0)
         las = lasio.read(tmpfile.name).df()
         y = las.GR.dropna()
         x = np.array(y.index)
-        return inpefa(y, x), x, file.name  # Using the original file name
+        return inpefa(y, x), x
 
 def main():
     st.title('Well Log Analysis')
@@ -47,15 +45,8 @@ def main():
 
     if uploaded_files:
         for uploaded_file in uploaded_files:
-            inpefa_log, x, filename = process_file(uploaded_file)
-            vislog(inpefa_log, x, filename)
-            # Create a DataFrame to save to CSV
-            df = pd.DataFrame(inpefa_log)
-            df['Depth'] = x
-            csv = df.to_csv(index=False)
-            # Remove the '.las' and replace with '.csv' for the download filename
-            download_filename = filename.replace('.las', '.csv')
-            st.download_button(label="Download CSV", data=csv, file_name=download_filename, mime='text/csv')
+            inpefa_log, x = process_file(uploaded_file)
+            vislog(inpefa_log, x)
     else:
         st.write('Please upload LAS files.')
 
